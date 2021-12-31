a609e0f7cd749211404c8dad179984c2895507a5
Abhishek kumar <abhishek1122338@gmail.com>
2022-01-01 00:18:40 +0530

    git

--- 30days6companiesProblems/GoldmanSachs(day1-5)/Print Anagrams Together.cpp
+++ 30days6companiesProblems/GoldmanSachs(day1-5)/Print Anagrams Together.cpp
@@ -30,3 +30,35 @@ class Solution{
   return ans;
 }
 };
+
+// Optimized Solution
+vector <vector <string>> ans{
+    map<map<char, int>, vector<string>> my_map;
+     
+    for(string str : my_list)
+    {
+        map<char, int> temp_map;
+        vector<string> temp_my_list;
+        for(int i = 0; i < str.length(); ++i)
+        {
+            ++temp_map[str[i]];
+        }
+        auto it = my_map.find(temp_map);
+        if (it != my_map.end())
+        {
+            it->second.push_back(str);
+        }
+        else
+        {
+            temp_my_list.push_back(str);
+            my_map.insert({ temp_map, temp_my_list });
+        }
+    }
+    vector<vector<string>> result;
+    for(auto it = my_map.begin();
+             it != my_map.end(); ++it)
+    {
+        result.push_back(it->second);
+    }
+    return result;
+}
